5fd6245a8d2b7138dccc88ea52f8a42325421ec9
ryans439 <ryans439@nycstudents.net>
2025-01-06 21:06:21 -0500

    dis

--- app/src/main.js
+++ app/src/main.js
@@ -1,24 +1,55 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const addItemBtn = document.getElementById('addItemBtn');
+const itemNameInput = document.getElementById('itemName');
+const itemPriceInput = document.getElementById('itemPrice');
+const itemQuantityInput = document.getElementById('itemQuantity');
+const inventoryList = document.getElementById('inventoryList');
+let inventory = [];
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function renderInventory() {
+    inventoryList.innerHTML = '';
+    for (let i = 0; i < inventory.length; i++) {
+        const item = inventory[i];
+        const card = document.createElement('div');
+        card.className = 'inventory-item';
+        card.innerHTML = `
+            <div>Name: ${item.name}</div>
+            <div>Price: $${item.price}</div>
+            <div>Quantity: ${item.quantity}</div>
+            <div>
+                <button onclick="updateItem(${i})">Update</button>
+                <button onclick="deleteItem(${i})">Delete</button>
+            </div>
+        `;
+        inventoryList.appendChild(card);
+    }
+}
 
-setupCounter(document.querySelector('#counter'))
+function addItem() {
+    const name = itemNameInput.value;
+    const price = itemPriceInput.value;
+    const quantity = itemQuantityInput.value;
+
+    if (name && price && quantity) {
+        const newItem = { name, price, quantity };
+        inventory.push(newItem);
+        renderInventory();
+        itemNameInput.value = '';
+        itemPriceInput.value = '';
+        itemQuantityInput.value = '';
+    }
+}
+
+function deleteItem(index) {
+    inventory.splice(index, 1);
+    renderInventory();
+}
+
+function updateItem(index) {
+    const newQuantity = prompt('Enter new quantity:', inventory[index].quantity);
+    if (newQuantity !== null) {
+        inventory[index].quantity = newQuantity;
+        renderInventory();
+    }
+}
+
+addItemBtn.addEventListener('click', addItem);
